1ed3d009ba7ccdccf5c618ec3723bed47e1e5db4
AdityaGourishetty <37544386+AdityaGourishetty@users.noreply.github.com>
2020-02-13 11:23:27 +0530

    Rename sixth_sense_robot.ino to Bot_Code.ino

---
+++ Bot_Code.ino
@@ -0,0 +1,62 @@
+char data='f';
+String motion;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(13,OUTPUT);
+Serial.begin(9600);
+}
+void Moverobot(String command)
+{
+  if(command=="f")
+  {
+    digitalWrite(10,HIGH);
+    digitalWrite(11,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,HIGH);
+  }
+  if(command=="s")
+  {
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,LOW);
+  }
+   if(command=="b")
+  {
+    digitalWrite(10,LOW);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+  }
+   if(command=="l")
+  {
+    digitalWrite(10,HIGH);
+    digitalWrite(11,LOW);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+  }
+   if(command=="r")
+  {
+    digitalWrite(10,LOW);
+    digitalWrite(11,HIGH);
+    digitalWrite(12,LOW);
+    digitalWrite(13,HIGH);
+  }
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while(Serial.available())
+{
+  data=Serial.read();
+  Serial.print(data);
+  motion=String(data);
+  Moverobot(motion);
+  delay(1000);
+}
+}
